e5020f23c7e4240195646048a9924683ef51634a
OviBhuiyan <ovibhuiyan43@gmail.com>
2017-04-18 02:52:43 +0600

    anchorScroll - js-21

--- AnghularJs/Scripts/Scripts.js
+++ AnghularJs/Scripts/Scripts.js
@@ -1,14 +1,10 @@
 ﻿/// <reference path="angular.min.js" />
 
-var app = angular
-          .module("myModule", [])
-          .controller("myController", function ($scope, stringService) {
-
-              $scope.transformString = function (input) {
-                  $scope.output = stringService.processString(input);
-              }
-
-          });
-
-
-
+var demoApp = angular.module("demoApp", [])
+                       .controller("demoController", function ($scope, $location, $anchorScroll) {
+                           $scope.scrollTo = function (scrollLocation) {
+                               $location.hash(scrollLocation);
+                               $anchorScroll.yOffset = 20;
+                               $anchorScroll();
+                           }
+                       });
